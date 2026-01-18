46e4a49ea9d516e5d75aadbab1bfcfac62e2a1bb
alvisac <martinezalvaradoanthonyisaac@gmail.com>
2026-01-18 00:51:28 -0600

    Initial Commit of this project

--- src/MyFirstProgram.java
+++ src/MyFirstProgram.java
@@ -1,7 +1,7 @@
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("hello, world!");
-        System.out.println("This just the beginning.");
+        System.out.println("This is just the beginning.");
         System.out.println();
         System.out.println("I will practice java every week.");
         System.out.println();
